db274ba291fa9ad050a043573f05a8fc8644bb15
ZackEndboss <github.com@zacky.org>
2023-09-12 19:25:23 +0200

    Update tatort_file_map.py
    Symlink added

--- renamer/src/tatort_file_map.py
+++ renamer/src/tatort_file_map.py
@@ -6,14 +6,19 @@ from renamer import Renamer
 SOURCE_DIR = "/Users/haiko/Documents/Temp/Filme/Tatort"
 DEST_DIR = "/Users/haiko/Documents/Temp/Tatort"
 DUPLICATE_DIR = "/Users/haiko/Documents/Temp/Tatort-Doppelt"
+SYMLINK = True
 
 def move(source, dest) :
     try :
         os.makedirs(os.path.dirname(dest))
     except :
         pass
-    os.rename(source, dest)
-    print(f"{source} -> {dest}")
+    if SYMLINK is True:
+        os.symlink(source, dest)
+        print(f"link: {source} -> {dest}")
+    else:
+        os.rename(source, dest)
+        print(f"move: {source} -> {dest}")
 
 renamer = Renamer()
 
@@ -24,8 +29,8 @@ for f in Path(SOURCE_DIR).iterdir():
             source = f"{f.parent}/{f.name}"
             dest = f"{DEST_DIR}/{new_name}"
             duplicate = f"{DUPLICATE_DIR}/{f.name}"
-            if not os.path.isfile(dest) :
-                print(f">>> {f.name} moved")
+            if not os.path.isfile(dest) and not os.path.islink(dest):
+                print(f">>> {f.name} moved/linked")
                 move(source, dest)
             else :
                 print(f"*** {f.name} duplicate")
